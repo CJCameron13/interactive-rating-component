c5c115104d5fe92e61442b1bddd11d24c432764e
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2023-07-24 11:34:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const selectRating = () => {
+    const number = document.querySelectorAll(".number");
+
+    number.forEach((number) => {
+        number.addEventListener("click", event => {
+            document.querySelector(".number.selected").classList.remove("selected");
+            event.currentTarget.classList.add("selected");
+    })
+    
+})
+}
+
+selectRating()
+
+const submitScore = () => {
+const submitButton = document.querySelector("#submit");
+const ratingBox = document.querySelector("#rating-state-container");
+const tyBox = document.querySelector("#ty-container");
+
+submitButton.addEventListener("click", () => {
+    console.log('submitted')
+    ratingBox.classList.add("hidden")
+    tyBox.classList.remove("hidden")
+})
+}
+
+submitScore()
+ 
+
+const populateScore = score => {
+     score = document.querySelector(".number.selected").textContent
+     const message = document.querySelector("#result")
+     console.log(score)
+     return message.textContent = `You selected ${score} out of 5`
+}
+
+populateScore()
